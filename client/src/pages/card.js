d0166ea947d7cbb1c155efad0e275caf6e92e4f4
jay <jayleungg@gmail.com>
2022-08-12 19:40:54 -0400

    rebase

--- client/src/pages/card.js
+++ client/src/pages/card.js
@@ -2,14 +2,7 @@ import { useEffect } from "react";
 import axios from "axios";
 
 const Card = (props) => {
-  return (
-    <div>
-      <li className="country-item" id={props.name}>
-        <h3>Country: {props.name}</h3>
-        <h5>Capital: {props.capital}</h5>
-      </li>
-    </div>
-  );
+  return <div></div>;
 };
 
 export default Card;
