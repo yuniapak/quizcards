d64b22e9f4fb7be6214c50ec1e295bdea0be7b43
jay <jayleungg@gmail.com>
2022-08-12 19:27:08 -0400

    .

--- client/src/pages/card.js
+++ client/src/pages/card.js
@@ -1,5 +1,8 @@
+import { useEffect } from "react";
+import axios from "axios";
+
 const Card = (props) => {
-  console.log(props);
+  console.log(props.subject + "asd");
 
   return <div>card</div>;
 };
