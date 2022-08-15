a17a22dccf5bf22c4326ed2320e753aa5f57b4f6
Yun <yuniya.pa98@gmail.com>
2022-08-15 12:23:34 -0400

    dropdown menu in add card fixed and working

--- client/src/pages/profile.js
+++ client/src/pages/profile.js
@@ -1,38 +1,30 @@
-import { useNavigate, Link } from "react-router-dom";
-import { useState, useEffect } from "react";
-import axios from "axios";
-import Card from "./card";
+import { useNavigate, Link } from 'react-router-dom'
+import { useState, useEffect } from 'react'
+import axios from 'axios'
+import Card from './card'
 
-const Profile = ({ setCardsObj, user, getCardbyType, setSubject, subject }) => {
-  let navigate = useNavigate();
-  const initialState = { value: "" };
-  const [userName, setUserName] = useState("");
-  const [loading, setLoading] = useState(true);
-  const [types, setTypes] = useState([]);
-
-  const getTypes = async () => {
-    try {
-      let result = await axios.get(
-        `http://localhost:3001/api/card/card/${user.id}`
-      );
-
-      setTypes(result.data.map(({ type }) => ({ label: type, value: type })));
-      console.log(result.data);
-      setLoading(false);
-    } catch (error) {
-      return error;
-    }
-  };
-
+const Profile = ({
+  setCardsObj,
+  user,
+  getCardbyType,
+  setSubject,
+  subject,
+  getTypes,
+  loading,
+  types
+}) => {
+  let navigate = useNavigate()
+  const initialState = { value: '' }
+  const [userName, setUserName] = useState('')
 
   const handleChange = (event) => {
-    event.preventDefault();
-    setSubject(event.target.value);
-    console.log(event.target.value);
-  };
+    event.preventDefault()
+    setSubject(event.target.value)
+    console.log(event.target.value)
+  }
 
   const handleSubmit = async (e) => {
-    e.preventDefault();
+    e.preventDefault()
     //calling axios on submit
 
     getCardbyType(subject)
@@ -48,9 +40,9 @@ const Profile = ({ setCardsObj, user, getCardbyType, setSubject, subject }) => {
   }
 
   useEffect(() => {
-    getTypes();
-    getUserName();
-  }, []);
+    getTypes()
+    getUserName()
+  }, [])
 
   return (
     <div>
@@ -90,7 +82,7 @@ const Profile = ({ setCardsObj, user, getCardbyType, setSubject, subject }) => {
         </div>
       </div>
     </div>
-  );
-};
+  )
+}
 
-export default Profile;
+export default Profile
