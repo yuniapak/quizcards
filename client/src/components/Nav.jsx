ac646f97bf640cbc72d7b75ea167e75c6d130956
jay <jayleungg@gmail.com>
2022-08-15 21:26:00 -0400

    rebase

--- client/src/components/Nav.jsx
+++ client/src/components/Nav.jsx
@@ -1,40 +1,44 @@
 import { NavLink } from "react-router-dom";
-function Nav({authenticated, user, handleLogOut}) {
-  let authenticatedOption
-  if(user){
+function Nav({ authenticated, user, handleLogOut }) {
+  let authenticatedOption;
+  if (user) {
     authenticatedOption = (
       <nav className="nav-bar">
-      <h1 className="nav-title">Quizards!</h1>
+        <h1 className="nav-title">Quizards!</h1>
+        <div className="nav-link">
+          <NavLink className="nav-link1" to="/Profile">
+            Profile |
+          </NavLink>
+          <NavLink className="nav-link1" to="/" onClick={handleLogOut}>
+            {" "}
+            LogOut
+          </NavLink>
+        </div>
+      </nav>
+    );
+  }
+
+  //-----Keep logo or revert?-----//
+  const publicOption = (
+    <nav className="nav-bar">
+      <h1 className="nav-title">
+        Quiz<span className="c">c</span>ards!
+      </h1>
       <div className="nav-link">
-        <NavLink className="nav-link1" to="/Profile">
-          Profile |
+        <NavLink className="nav-link1" to="/">
+          Home |
         </NavLink>
-        <NavLink className="nav-link1" to="/" onClick={handleLogOut}>
+        <NavLink className="nav-link1" to="/Register">
           {" "}
-          LogOut
+          Sign up |
+        </NavLink>
+        <NavLink className="nav-link1" to="/Login">
+          {" "}
+          Login
         </NavLink>
       </div>
     </nav>
-    )
-  }
-  const publicOption =( 
-    <nav className="nav-bar">
-    <h1 className="nav-title">Quizards!</h1>
-    <div className="nav-link">
-      <NavLink className="nav-link1" to="/">
-        Home |
-      </NavLink>
-      <NavLink className="nav-link1" to="/Register">
-        {" "}
-        Sign up |
-      </NavLink>
-      <NavLink className="nav-link1" to="/Login">
-        {" "}
-        Login
-      </NavLink>
-    </div>
-  </nav>
-  )
+  );
   return (
     <div>{authenticated && user ? authenticatedOption : publicOption}</div>
   );
