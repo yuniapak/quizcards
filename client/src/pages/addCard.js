93a982db3e62ecae15b67e6563c3b57d30798f1c
Yun <yuniya.pa98@gmail.com>
2022-08-16 11:32:39 -0400

    conflict fixed

--- client/src/pages/addCard.js
+++ client/src/pages/addCard.js
@@ -40,60 +40,77 @@ const AddCard = ({ user, loading, types }) => {
 
   return (
     <div className="main-div">
-      <form onSubmit={handleSubmit}>
-        <label id="form-select" htmlFor="SubjectType">
-          Add Card
+      <div className="Addcard">
+        <form onSubmit={handleSubmit}>
+          <label id="form-select" htmlFor="SubjectType">
+            <br></br>
+          </label>
           <br></br>
-        </label>
-        <br></br>
-        <select
-          id="value"
-          name="type"
-          onChange={handleChange}
-          value={newCard.type}
-          disabled={loading}
-        >
-          <option value="" disabled hidden>
-            Selection
-          </option>
-          {types.map(({ value, label }) => (
-            <option value={value}>{label}</option>
-          ))}
-
-          <option value={newCard.type}>New Subject</option>
-        </select>
-        <br></br>
-        <input
-          onChange={handleChange}
-          name="type"
-          type="text"
-          placeholder="Add new Subject"
-          value={newCard.type}
-        ></input>
-        <input
-          onChange={handleChange}
-          name="question"
-          type="text"
-          placeholder="Your question?"
-          value={newCard.question}
-          required
-        />
-        <input
-          onChange={handleChange}
-          name="answer"
-          type="text"
-          placeholder="Answer"
-          value={newCard.answer}
-          required
-        />
-        <button
-          type="submit"
-          disabled={!newCard.question || !newCard.answer}
-          // onClick={handleSubmit()}
-        >
-          Add
-        </button>
-      </form>
+          <select
+            id="value"
+            name="type"
+            onChange={handleChange}
+            value={newCard.type}
+            disabled={loading}
+          >
+            <option value="" disabled hidden>
+              Selection
+            </option>
+            {types.map(({ value, label }) => (
+              <option value={value}>{label}</option>
+            ))}
+            <option value={newCard.type}>NewSubject</option>
+          </select>
+          <br></br>
+          <br></br>
+          <br></br>
+          New Subject<br></br>
+          <input
+            className="addCard-input"
+            onChange={handleChange}
+            name="type"
+            type="text"
+            placeholder="Add new Subject"
+            //disabled={id !== ''}
+            value={newCard.type}
+          ></input>
+          <br></br>
+          Question
+          <br></br>
+          <input
+            className="addCard-input"
+            onChange={handleChange}
+            name="question"
+            type="text"
+            placeholder="Your question?"
+            value={newCard.question}
+            required
+          />
+          <br></br>
+          Answer
+          <br></br>
+          <input
+            rows="6"
+            cols="50"
+            className="addCard-input"
+            onChange={handleChange}
+            name="answer"
+            type="text"
+            placeholder="Answer"
+            value={newCard.answer}
+            required
+          />
+          <button
+            className="addCard-btn"
+            type="submit"
+            disabled={!newCard.question || !newCard.answer}
+            // onClick={handleSubmit()}
+          >
+            Submit
+          </button>
+        </form>
+      </div>
+      >>>>>>> 4f30548 (addcard styled)
     </div>
   )
 }
