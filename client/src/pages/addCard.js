a3bb1fb22d60bcf1a37aeabd82ca52a9e352e891
Yun <yuniya.pa98@gmail.com>
2022-08-16 10:19:07 -0400

    small changes

--- client/src/pages/addCard.js
+++ client/src/pages/addCard.js
@@ -1,42 +1,42 @@
-import axios from "axios";
-import { useEffect, useState } from "react";
-import { useNavigate } from "react-router-dom";
+import axios from 'axios'
+import { useEffect, useState } from 'react'
+import { useNavigate } from 'react-router-dom'
 
 const AddCard = ({ user, loading, types }) => {
-  console.log(user);
+  console.log(user)
   const [newCard, setNewCard] = useState({
-    type: "",
-    question: "",
-    answer: "",
-  });
+    type: '',
+    question: '',
+    answer: ''
+  })
 
   const handleChange = (event) => {
-    setNewCard({ ...newCard, [event.target.name]: event.target.value });
-    console.log(newCard);
-  };
+    setNewCard({ ...newCard, [event.target.name]: event.target.value })
+    console.log(newCard)
+  }
 
   const addNewCard = async (cardData) => {
     const res = await axios.post(
       `http://localhost:3001/api/card/${user.id}`,
       cardData
-    );
-    console.log(res.cardData);
-  };
+    )
+    console.log(res.cardData)
+  }
 
-  let navigate = useNavigate();
+  let navigate = useNavigate()
 
   const handleSubmit = async (e) => {
-    e.preventDefault();
-    await addNewCard(newCard);
-    console.log(newCard);
+    e.preventDefault()
+    await addNewCard(newCard)
+    console.log(newCard)
     setNewCard({
-      type: "",
-      question: "",
-      answer: "",
-    });
+      type: '',
+      question: '',
+      answer: ''
+    })
     //navigate for now, maybe change to different page later
-    navigate("/Profile");
-  };
+    navigate('/Profile')
+  }
 
   return (
     <div className="main-div">
@@ -59,7 +59,8 @@ const AddCard = ({ user, loading, types }) => {
           {types.map(({ value, label }) => (
             <option value={value}>{label}</option>
           ))}
-          <option value={newCard.type}>NewSubject</option>
+
+          <option value={newCard.type}>New Subject</option>
         </select>
         <br></br>
         <input
@@ -67,7 +68,6 @@ const AddCard = ({ user, loading, types }) => {
           name="type"
           type="text"
           placeholder="Add new Subject"
-          //disabled={id !== ''}
           value={newCard.type}
         ></input>
         <input
@@ -95,7 +95,7 @@ const AddCard = ({ user, loading, types }) => {
         </button>
       </form>
     </div>
-  );
-};
+  )
+}
 
-export default AddCard;
+export default AddCard
