7a4b7e729de138aee60d3e951caab2474aeacf1b
jay <jayleungg@gmail.com>
2022-08-17 12:55:03 -0400

    scoreup

--- client/src/pages/quiz.js
+++ client/src/pages/quiz.js
@@ -1,65 +1,78 @@
-import { useState, useEffect } from 'react'
-import axios from 'axios'
+import { useState, useEffect } from "react";
+import axios from "axios";
 
+const Quiz = ({ cardsObj, subject }) => {
+  const [currentQuestion, setQuestion] = useState(0);
+  const [currentScore, setScore] = useState(0);
+  const [popup, setPopup] = useState(false);
+  const [idOfCard, setIdOfCard] = useState(null);
 
-const Quiz = ({cardsObj}) => {
+  const showAnswer = (e) => {
+    setPopup(true);
+    setIdOfCard(e.target.id);
+    console.log(idOfCard);
+  };
 
-  
+  const addScore = () => {
+    setScore(currentScore + 1);
+    setPopup(false);
+  };
 
-  const [currentQuestion, setQuestion] = useState(0)
-  const [currentScore, setScore] = useState(0)
+  useEffect(() => {}, []);
 
+  // const nextQuestion = currentQuestion + 1;
+  // if(nextQuestion < question.length){
+  //   setQuestion(nextQuestion)
+  // }else{
+  //   setScore(true)
+  // }
 
-useEffect (()=>{
+  // const buttonClicked = () =>{
+  // if (isCorrect === true){
+  //   setScore(currentScore + 1)
+  // }
+  // }
 
-},[])
-
-
-
-
-// const nextQuestion = currentQuestion + 1;
-// if(nextQuestion < question.length){
-//   setQuestion(nextQuestion)
-// }else{
-//   setScore(true)
-// }
-
-
-// const buttonClicked = () =>{
-// if (isCorrect === true){
-//   setScore(currentScore + 1)
-// }
-// }
-
-
-
-  return( 
-  
-  <div>
-
-    <h2>Quizards Quizly Quiz Game!</h2>
-    <p>Do you know the answers to these questions?</p>
- 
+  return (
     <div>
-    {cardsObj.map((card)=>{
-  <li key = {card.id}>
-    <h3>{card.question}</h3>
-    <p>{card.answer}</p>
-  </li>
-})}
-
-     <h2></h2>
-     <button></button>
-
+      <h2>Quizards Quizly Quiz Game!</h2>
+      <p>Do you know the answers to these questions?</p>
+      <h3>{subject}</h3>
+      <div>
+        {cardsObj.map((card) => (
+          <div key={card.id}>
+            <h3>{card.question}</h3>
+            <input type="text" value="Answer..." />
+            <div>
+              <button id={card.Id} onClick={showAnswer}>
+                Check Answer
+              </button>
+              {popup && (
+                <div className="answer-container">
+                  Answer:
+                  {card.answer}
+                  <button
+                    onClick={() => setPopup(false)}
+                    className="answer-close-btn"
+                  >
+                    Close
+                  </button>
+                  <button onClick={addScore} className="yes-btn">
+                    Yes
+                  </button>
+                  <button className="no-btn">No</button>
+                </div>
+              )}
+            </div>
+          </div>
+        ))}
+      </div>
+      <div className="score">You scored {currentScore} out of </div>
+      {/* <div>
+        <button onClick={() => setPopup(!popup)}>'hello'</button>
+        {popup && <div>content</div>}
+      </div> */}
     </div>
- 
-
-
-    <div className='score'>You scored {currentScore} out of </div>
-   
-  </div>
-
-)};
-
-
-export default Quiz
+  );
+};
+export default Quiz;
