bfc40c2a1e0e3504d79c5b6b72118a4bf96dd7b3
Yun <yuniya.pa98@gmail.com>
2022-08-17 10:30:39 -0400

    question shows on click

--- client/src/pages/card.js
+++ client/src/pages/card.js
@@ -1,35 +1,37 @@
-import { useNavigate } from "react-router-dom";
-import axios from "axios";
-import { useEffect, useState } from "react";
+import { useNavigate } from 'react-router-dom'
+import axios from 'axios'
+import { useEffect, useState } from 'react'
 
 const Card = ({ cardsObj, getCardbyType, subject }) => {
-  const [showCard, setShowCard] = useState(false);
-  cardsObj.map((card) => console.log(card));
-  let navigate = useNavigate();
+  const [showCard, setShowCard] = useState(false)
+  const [idOfCard, setIdOfCard] = useState('')
+  cardsObj.map((card) => console.log(card))
+  let navigate = useNavigate()
 
   const editCard = (card) => {
-    navigate(`${card.Id}`, { state: { card: card } });
-    console.log(cardsObj);
-  };
+    navigate(`${card.Id}`, { state: { card: card } })
+    console.log(cardsObj)
+  }
   useEffect(() => {
-    getCardbyType(subject);
-  }, []);
-
-  useEffect(() => {
-    getCardbyType(subject);
-  }, []);
+    getCardbyType(subject)
+  }, [])
 
   const deleteCard = async (id) => {
     let res = await axios
       .delete(`http://localhost:3001/api/card/${id}`)
-      .catch((error) => console.log(error));
-    console.log(res);
-    await getCardbyType(subject);
-  };
+      .catch((error) => console.log(error))
+    console.log(res)
+    await getCardbyType(subject)
+  }
+
+  const cardShow = (e) => {
+    setShowCard(true)
+    setIdOfCard(e.target.id)
+  }
 
   return (
     <div className="main-div">
-      <button className="add-btn" onClick={() => navigate("/addCard")}>
+      <button className="add-btn" onClick={() => navigate('/addCard')}>
         Add New Card
       </button>
       <div className="card-grid">
@@ -37,8 +39,7 @@ const Card = ({ cardsObj, getCardbyType, subject }) => {
           <div className="quiz-card" key={card.id}>
             <h1>{card.type.toUpperCase()}</h1>
             <h1 className="quiz-question">{card.question.toUpperCase()}?</h1>
-
-            {showCard ? (
+            {showCard && idOfCard == card.id ? (
               <h1 className="quiz-answer">Answer: {card.answer}</h1>
             ) : null}
             <button className="quiz-edit-btn" onClick={() => editCard(card)}>
@@ -50,12 +51,14 @@ const Card = ({ cardsObj, getCardbyType, subject }) => {
             >
               Delete
             </button>
-            <button onClick={() => setShowCard(!showCard)}>Show Card</button>
+            <button id={card.id} onClick={cardShow}>
+              Show Card
+            </button>
           </div>
         ))}
       </div>
     </div>
-  );
-};
+  )
+}
 
-export default Card;
+export default Card
