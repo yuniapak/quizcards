645fa3f6d21826734fcc1e8c652a8c55dd720a8d
Yun <yuniya.pa98@gmail.com>
2022-08-17 10:07:35 -0400

    changes before pull

--- client/src/pages/quiz.js
+++ client/src/pages/quiz.js
@@ -1,31 +1,25 @@
-// import {useState, useEffect} from 'react'
-// import axios from 'axios'
+import { useState, useEffect } from 'react'
+import axios from 'axios'
 
-// const Quiz = () => {
+const Quiz = () => {
+  const [currentQuestion, setQuestion] = useState(0)
+  const [currentScore, setScore] = useState(0)
 
-//   const [currentQuestion, setQuestion] = useState(0)
-//   const [currentScore, setScore] = useState(0)
+  //   const buttonClicked = () => {
+  //     if (isCorrect === true) {
+  //       setScore(currentScore + 1)
+  //     }
+  //   }
+  const nextQuestion = currentQuestion + 1
 
-// const buttonClicked = () =>{
-// if (isCorrect === true){
-//   setScore(currentScore + 1)
-// }
-// }
-// const nextQuestion = currentQuestion + 1;
+  const settleScore = () => {}
 
-// const settleScore = ()=>{
+  return (
+    <div>
+      <h2>Quizards Quizly Quiz Game!</h2>
+      <p>Do you know the answers to these questions?</p>
+    </div>
+  )
+}
 
-// }
-
-//   return(
-
-//   <div>
-
-//     <h2>Quizards Quizly Quiz Game!</h2>
-//     <p>Do you know the answers to these questions?</p>
-
-//   </div>
-
-// )};
-
-// export default Quiz;
+export default Quiz
